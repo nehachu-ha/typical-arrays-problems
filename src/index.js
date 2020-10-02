58498d3caa0f56ba61a210201a379dbc9be75104
nehachu-ha <nehachu-ha@list.ru>
2020-10-02 23:12:45 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+    return  array && array.length ? array.sort((a, b) => a - b )[0] : 0 ;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return array && array.length ? array.sort((a, b) => b - a )[0] : 0 ;;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array && array.length ? array.reduce((acc, c) => acc + c, 0) / array.length : 0;
 }
